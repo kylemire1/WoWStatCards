9a51ca9745c45a1be968cec4a9d228d0117ac38f
Kyle Lemire <kyle@elykinnovation.com>
2022-09-18 19:36:59 -0500

    Add Cors

--- WoWStatCards.API/Startup.cs
+++ WoWStatCards.API/Startup.cs
@@ -8,10 +8,12 @@ using WoWStatCards.API;
 public class Startup
 {
     private readonly IConfiguration _configuration;
+    private readonly string _myAllowSpecificOrigins = "_myAllowSpecificOrigins";
 
     public Startup(IConfiguration configuration)
     {
         _configuration = configuration;
+
     }
 
     public void ConfigureServices(IServiceCollection services)
@@ -28,12 +30,19 @@ public class Startup
         services.AddSingleton<CharacterStatClient>();
         services.AddScoped<IStatCardRepository, StatCardRepository>();
         services.AddAutoMapper(typeof(MappingConfig));
-
+        services.AddCors(options =>
+        {
+            options.AddPolicy(name: _myAllowSpecificOrigins, policy =>
+            {
+                policy.WithOrigins("http://localhost:3000");
+            });
+        });
     }
     public void Configure(WebApplication app, IWebHostEnvironment env)
     {
 
         app.UseRouting();
+        app.UseCors(_myAllowSpecificOrigins);
         app.UseEndpoints(x => x.MapControllers());
         // Configure the HTTP request pipeline.
         if (env.IsDevelopment())
